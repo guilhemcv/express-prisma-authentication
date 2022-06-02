b97eb43225ffa8d46d639c935ca6002266f15dff
Guilhem SEYVET <guilhemcvdev@outlook.com>
2022-06-02 11:47:12 +0200

    adding the routes in the router.js file

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const user = require("./controllers/auth.controller");
+const auth = require("./middleware/auth");
 const { ItemController } = require("./controllers");
 
 const router = express.Router();
@@ -9,5 +11,12 @@ router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
+router.post("/auth", user.register);
+router.post("/auth/login", user.login);
+router.post("/test/login", auth, (req, res) => {
+  res.json({
+    message: "User correctly connected",
+  });
+});
 
 module.exports = router;
